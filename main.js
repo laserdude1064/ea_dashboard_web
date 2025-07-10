3b2cef58b5eeebc9b62e5ad7646ddd6036413b21
laserdude1064 <eric.syrbe@freenet.de>
2025-07-10 14:01:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 console.log("📡 main.js geladen");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// TODO: Firebase-Initialisierung und Datenabruf einfügen
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1cqUCWwACeFYFFZ7MyIOweamKZ8PnNKU",
+  authDomain: "ea-dashboard-636cf.firebaseapp.com",
+  databaseURL: "https://ea-dashboard-636cf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ea-dashboard-636cf",
+  storageBucket: "ea-dashboard-636cf.firebasestorage.app",
+  messagingSenderId: "602990579998",
+  appId: "1:602990579998:web:c6775bcae19a8afba5b90f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 🔍 Beispiel: Alle Dokumente aus 'ea_monitoring' lesen
+async function fetchData() {
+  const snapshot = await getDocs(collection(db, "ea_monitoring"));
+  snapshot.forEach((doc) => {
+    console.log("📦 Eintrag:", doc.data());
+  });
+}
+
+fetchData();
