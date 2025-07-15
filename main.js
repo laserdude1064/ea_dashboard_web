ec9375fa4aa6f628c1e7c482dbba7e9fdf730b2d
laserdude1064 <eric.syrbe@freenet.de>
2025-07-15 13:55:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import 'chartjs-adapter-luxon';
-
+ 
 console.log("📡 main.js geladen");
 
 document.addEventListener("DOMContentLoaded", () => {
