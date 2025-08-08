a7c76fa2da796c45f0ba7ce946c56fee941eeabf
laserdude1064 <eric.syrbe@freenet.de>
2025-08-08 13:44:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -692,7 +692,7 @@ function renderMultiEAStatusTable(dataList) {
   tableHead.innerHTML = "";
 
   const fieldOrder = [
-    "symbol", "received_at", "TimeFilterActive", "buy_count", "sell_count", "lotsize", "TPFO", "TP", "SL", "waitmins", "BuyGrid", "SellGrid",
+    "symbol", "received_at", "TimeFilterActive", "NoTrade", "buy_count", "sell_count", "lotsize", "TPFO", "TP", "SL", "waitmins", "BuyGrid", "SellGrid",
     "Buy_BB", "Buy_RSI", "Buy_MACD", "Buy_margin",
     "Buy_maxcount", "Buy_tickmax", "Buy_ATR",
     "Sell_BB", "Sell_RSI", "Sell_MACD", "Sell_margin",
